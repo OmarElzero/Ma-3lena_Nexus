88a194ff691131dc810ee34ebef98d82ef0b4295
AmrKhaled35 <amrk6965@gmail.com>
2025-09-03 15:32:40 +0300

    Add : App Routes

--- Malaena/src/App.tsx
+++ Malaena/src/App.tsx
@@ -1,7 +1,21 @@
-import React from 'react'
-
-export default function App() {
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/landing";
+import LoginPage from "./pages/auth/login";
+import SignUpPage from "./pages/auth/signup";
+import DashboardPage from "./pages/dashboard";
+import ProfilePage from "./pages/profile";
+function App() {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} /> 
+        <Route path="/profile" element={<ProfilePage />} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
